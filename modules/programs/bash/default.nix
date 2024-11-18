e4064bdf7824ad374c243b84acc8337396afed18
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-18 08:34:06 +0700

    bash: Handle bash.bashrc

--- modules/programs/bash/default.nix
+++ modules/programs/bash/default.nix
@@ -4,6 +4,53 @@ with lib;
 
 let
   cfg = config.programs.bash;
+
+  bashrc = ''
+    # /etc/bashrc: DO NOT EDIT -- this file has been generated automatically.
+    # This file is read for interactive shells.
+
+    [ -r "/etc/bashrc_$TERM_PROGRAM" ] && . "/etc/bashrc_$TERM_PROGRAM"
+
+    # Only execute this file once per shell.
+    if [ -n "$__ETC_BASHRC_SOURCED" -o -n "$NOSYSBASHRC" ]; then return; fi
+    __ETC_BASHRC_SOURCED=1
+
+    if [ -z "$__NIX_LINUX_SET_ENVIRONMENT_DONE" ]; then
+      . ${config.system.build.setEnvironment}
+    fi
+
+    # Return early if not running interactively, but after basic nix setup.
+    [[ $- != *i* ]] && return
+
+    # Make bash check its window size after a process completes
+    shopt -s checkwinsize
+
+    ${config.system.build.setAliases.text}
+
+    ${config.environment.interactiveShellInit}
+    ${cfg.interactiveShellInit}
+
+    ${optionalString cfg.completion.enable ''
+      if [ "$TERM" != "dumb" ]; then
+        source "${cfg.completion.package}/etc/profile.d/bash_completion.sh"
+
+        nullglobStatus=$(shopt -p nullglob)
+        shopt -s nullglob
+        for p in $NIX_PROFILES; do
+          for m in "$p/etc/bash_completion.d/"*; do
+            source $m
+          done
+        done
+        eval "$nullglobStatus"
+        unset nullglobStatus p m
+      fi
+    ''}
+
+    # Read system-wide modifications.
+    if test -f /etc/bash.local; then
+      source /etc/bash.local
+    fi
+  '';
 in
 
 {
@@ -53,52 +100,8 @@ in
         "/share/bash-completion/completions"
       ];
 
-    environment.etc."bashrc".text = ''
-      # /etc/bashrc: DO NOT EDIT -- this file has been generated automatically.
-      # This file is read for interactive shells.
-
-      [ -r "/etc/bashrc_$TERM_PROGRAM" ] && . "/etc/bashrc_$TERM_PROGRAM"
-
-      # Only execute this file once per shell.
-      if [ -n "$__ETC_BASHRC_SOURCED" -o -n "$NOSYSBASHRC" ]; then return; fi
-      __ETC_BASHRC_SOURCED=1
-
-      if [ -z "$__NIX_LINUX_SET_ENVIRONMENT_DONE" ]; then
-        . ${config.system.build.setEnvironment}
-      fi
-
-      # Return early if not running interactively, but after basic nix setup.
-      [[ $- != *i* ]] && return
-
-      # Make bash check its window size after a process completes
-      shopt -s checkwinsize
-
-      ${config.system.build.setAliases.text}
-
-      ${config.environment.interactiveShellInit}
-      ${cfg.interactiveShellInit}
-
-      ${optionalString cfg.completion.enable ''
-        if [ "$TERM" != "dumb" ]; then
-          source "${cfg.completion.package}/etc/profile.d/bash_completion.sh"
-
-          nullglobStatus=$(shopt -p nullglob)
-          shopt -s nullglob
-          for p in $NIX_PROFILES; do
-            for m in "$p/etc/bash_completion.d/"*; do
-              source $m
-            done
-          done
-          eval "$nullglobStatus"
-          unset nullglobStatus p m
-        fi
-      ''}
-
-      # Read system-wide modifications.
-      if test -f /etc/bash.local; then
-        source /etc/bash.local
-      fi
-    '';
+    environment.etc."bashrc".text = bashrc;
+    environment.etc."bash.bashrc".text = bashrc;
 
     environment.etc."bashrc".knownSha256Hashes = [
       "444c716ac2ccd9e1e3347858cb08a00d2ea38e8c12fdc5798380dc261e32e9ef"  # macOS
@@ -107,6 +110,7 @@ in
       "87809e49352a19224ab3ecf88d5f3869581e8c3391331570b0d1febb8800578f"  # official Nix installer
       "08ffbf991a9e25839d38b80a0d3bce3b5a6c84b9be53a4b68949df4e7e487bb7"  # DeterminateSystems installer
     ];
+    environment.etc."bash.bashrc".knownSha256Hashes = config.environment.etc."bashrc".knownSha256Hashes;
 
   };
 }
